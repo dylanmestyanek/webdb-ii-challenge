e4529756f9f524f1ceb0d81265235b18412f3dac
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-05 13:51:52 -0600

    Adjusted and rebuilt initial cars table that was unorganized.

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
       return knex('cars').insert([
         { VIN: 123459876, make: "Toyota", model: "Corolla", mileage: 123456 },
         { VIN: 987654321, make: "Kia", model: "Rio", mileage: 123456, title: "clean" },
-        { VIN: 123456789, make: "Cheverolet", model: "Silverado", mileage: 254000, title: "clean" }
+        { VIN: 123456789, make: "Chevrolet", model: "Silverado", mileage: 254000, title: "clean" }
       ]);
     });
 };
